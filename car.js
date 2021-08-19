d34a0a26dd2055440a8603a36f7928258dcfa493
Akash Kumar Singh <67006219+StarX0406@users.noreply.github.com>
2021-08-19 15:35:13 +0530

    Update car.js

--- car.js
+++ car.js
@@ -1,8 +1,8 @@
-const btnNew = document.querySelector('.btn--new');
-const btnNext = document.querySelector('.btn--next');
-const currentPetrol = document.querySelector('.petrol');
-const currentDistance = document.querySelector('.distance');
-const board = document.querySelector('.board');
+const btnNew = document.querySelector(".btn--new");
+const btnNext = document.querySelector(".btn--next");
+const currentPetrol = document.querySelector(".petrol");
+const currentDistance = document.querySelector(".distance");
+const board = document.querySelector(".board");
 
 let distanceTravel = 0;
 let remeningPetrol = 50;
@@ -16,7 +16,7 @@ for (let i = 0; i < 6; i++) {
   petrolPump.push(pump);
 }
 
-btnNext.addEventListener('click', function () {
+btnNext.addEventListener("click", function () {
   if (playing) {
     move++;
 
@@ -29,27 +29,29 @@ btnNext.addEventListener('click', function () {
 
     let petrolConsume = travel * 2;
     remeningPetrol = remeningPetrol - petrolConsume;
+    if (remeningPetrol < 0) {
+      remeningPetrol = 0;
+    }
     currentPetrol.textContent = remeningPetrol;
-    console.log(
-      `move ${move} - car at ${distanceTravel}, petrol remaining ${remeningPetrol}.`
-    );
+
+    console.log(`move ${move} - car at ${distanceTravel}, petrol remaining ${remeningPetrol}.`);
 
     if (remeningPetrol <= 0 && distanceTravel != 100) {
-      board.textContent = 'You Lost';
-      console.log('You Lost');
+      board.textContent = "You Lost";
+      console.log("You Lost");
       playing = false;
     }
     if (remeningPetrol >= 0 && distanceTravel >= 100) {
-      board.textContent = 'You Won';
-      console.log('You won');
+      board.textContent = "You Won";
+      console.log("You won");
       playing = false;
     }
   }
   if (!playing) {
-    console.log('restart again ');
+    console.log("restart again ");
   }
 });
 
-btnNew.addEventListener('click', function () {
+btnNew.addEventListener("click", function () {
   window.location.reload();
 });
